bbc3c5580e802ac6b7e3f33da90cdcdcb9c323da
yeonic <bellopenerr@gmail.com>
2022-09-20 21:52:42 +0900

    csvman: implement csv reader

--- 1. bpt/source/fileman/csvman.py
+++ 1. bpt/source/fileman/csvman.py
@@ -1,6 +1,18 @@
 import csv
+from queue import Queue
 
 
 class CsvMan:
-    __slots__ = []
-    
+    __slots__ = ["input_file", "task_queue"]
+
+    def __init__(self, input_file):
+        self.input_file = input_file
+        self.task_queue: Queue = Queue()
+
+    def read_input(self):
+        csv_file = open(self.input_file, 'r')
+        return self.record_input(csv_file)
+
+    def record_input(self, csv_file):
+        for line in csv_file:
+            self.task_queue.put(line.rstrip())
